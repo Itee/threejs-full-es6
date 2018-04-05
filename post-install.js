b99adaad33587114cbbc9bee6ab816a5f314d6f9
Tristan VALCKE <valcketristan@gmail.com>
2018-04-05 21:04:41 +0200

    Add post-install deprecation message

---
+++ post-install.js
@@ -0,0 +1,11 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Todo
+ *
+ * @example Todo
+ *
+ */
+
+console.error('DEPRECATED: This project has been renamed to three-full. Install using three-full instead.')
